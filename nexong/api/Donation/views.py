3d3ba2b5ea34d77e13ad52262b0e6cddc43b3161
MRomalde <MRomalde@users.noreply.github.com>
2024-03-07 10:57:32 +0000

    refactor: Format code with Black

--- nexong/api/Donation/views.py
+++ nexong/api/Donation/views.py
@@ -7,6 +7,7 @@ from .. import permissions
 import csv
 from django.http import HttpResponse
 
+
 class DonationApiViewSet(ModelViewSet):
     queryset = Donation.objects.all()
     http_method_names = ["get", "post", "put", "delete"]
@@ -16,10 +17,11 @@ class DonationApiViewSet(ModelViewSet):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
+
 def DonationsExportToCsv(request):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="Datos_Donaciones.csv"'
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="Datos_Donaciones.csv"'
 
     # Retrieve data from your model
     queryset = Donation.objects.all()
@@ -28,10 +30,20 @@ def DonationsExportToCsv(request):
     writer = csv.writer(response)
 
     # Write the header row
-    writer.writerow(['Cantidad', ' Frecuencia', ' QuotaExtensionDocument', ' Titular', ' Fecha'])
+    writer.writerow(
+        ["Cantidad", " Frecuencia", " QuotaExtensionDocument", " Titular", " Fecha"]
+    )
 
     # Write data rows
     for donation in queryset:
-        writer.writerow([donation.quantity, donation.frequency, donation.quota_extension_document, donation.holder, donation.date])  # Replace field1, field2, etc. with your actual field names
+        writer.writerow(
+            [
+                donation.quantity,
+                donation.frequency,
+                donation.quota_extension_document,
+                donation.holder,
+                donation.date,
+            ]
+        )  # Replace field1, field2, etc. with your actual field names
 
     return response
--- src/urls.py
+++ src/urls.py
@@ -3,4 +3,8 @@ from django.urls import path, include
 from nexong.api.routers import router_api
 from nexong.api.Donation.views import *
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router_api.urls)), path("api/donation/export", DonationsExportToCsv, name="export-donation-to-csv")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router_api.urls)),
+    path("api/donation/export", DonationsExportToCsv, name="export-donation-to-csv"),
+]
