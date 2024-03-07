96b1627dda1b7283888f2b31b10c6d41aa685c4b
mromalde <mromalde@gmail.com>
2024-03-07 11:54:21 +0100

    feat: Data exported from all donations

--- nexong/api/Donation/views.py
+++ nexong/api/Donation/views.py
@@ -4,15 +4,34 @@ from rest_framework import status
 from ...models import *
 from .donationSerializer import DonationSerializer
 from .. import permissions
-
+import csv
+from django.http import HttpResponse
 
 class DonationApiViewSet(ModelViewSet):
     queryset = Donation.objects.all()
     http_method_names = ["get", "post", "put", "delete"]
     serializer_class = DonationSerializer
-    permission_classes = [permissions.isAdmin]
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+def DonationsExportToCsv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="Datos_Donaciones.csv"'
+
+    # Retrieve data from your model
+    queryset = Donation.objects.all()
+
+    # Create a CSV writer object
+    writer = csv.writer(response)
+
+    # Write the header row
+    writer.writerow(['Cantidad', ' Frecuencia', ' QuotaExtensionDocument', ' Titular', ' Fecha'])
+
+    # Write data rows
+    for donation in queryset:
+        writer.writerow([donation.quantity, donation.frequency, donation.quota_extension_document, donation.holder, donation.date])  # Replace field1, field2, etc. with your actual field names
+
+    return response
--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from nexong.api.routers import router_api
+from nexong.api.Donation.views import *
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router_api.urls))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router_api.urls)), path("api/donation/export", DonationsExportToCsv, name="export-donation-to-csv")]
