da9577fdbb1055a1fe51168fdb47a4752217b7c0
manortbla <manortbla@users.noreply.github.com>
2024-03-13 11:17:04 +0000

    refactor: Format code with Black

--- src/urls.py
+++ src/urls.py
@@ -5,20 +5,20 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Nexong Docs",
-      default_version='v1',
-      description="Backend documentation",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="margondia22@alum.us.es"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
+    openapi.Info(
+        title="Nexong Docs",
+        default_version="v1",
+        description="Backend documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="margondia22@alum.us.es"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router_api.urls)),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0))]
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0)),
+]
