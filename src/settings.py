d17f24d5bcbdbef2b7d17871fee1b32ed659b3c5
manortbla <manortbla@alum.us.es>
2024-03-13 12:08:49 +0100

    feat: Backend documentation with Swagger

--- src/settings.py
+++ src/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "nexong",
     "corsheaders",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from nexong.api.routers import router_api
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router_api.urls))]
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Nexong Docs",
+      default_version='v1',
+      description="Backend documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="margondia22@alum.us.es"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router_api.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0))]
