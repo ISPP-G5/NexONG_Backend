77987d1682806938234de4512cf428360073be78
Ahydul <ahydul1@gmail.com>
2024-03-18 12:25:01 +0100

    fix: missing import

--- nexong/api/Authentication/views.py
+++ nexong/api/Authentication/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from ...models import *
 from .authSerializer import *
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 def process_instance(serializer_class, instance, data):
@@ -98,9 +99,9 @@ class LogoutAndBlacklistRefreshTokenForUserView(APIView):
                 refresh_token = serializer.validated_data["refresh_token"]
                 token = RefreshToken(refresh_token)
                 token.blacklist()
-                return Response(status=status.HTTP_205_RESET_CONTENT)
-            except Exception:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+                return Response(status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response(e.args, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
