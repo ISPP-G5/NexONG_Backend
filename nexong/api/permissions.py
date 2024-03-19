d340c68944e1b18042a87ce825120ee8c48d9773
JuanluRM <JuanluRM@users.noreply.github.com>
2024-03-19 22:30:42 +0000

    refactor: Format code with Black

--- nexong/api/permissions.py
+++ nexong/api/permissions.py
@@ -3,19 +3,18 @@ from rest_framework.permissions import BasePermission, DjangoModelPermissions
 
 class isAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-
         if request.user.is_authenticated:
             if request.method in ("GET", "HEAD", "OPTIONS"):
                 return True
             else:
-                return request.user.role=="ADMIN"
+                return request.user.role == "ADMIN"
         else:
             return False
 
 
 class isAdminOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.role=="ADMIN")
+        return bool(request.user and request.user.role == "ADMIN")
 
 
 class FullDjangoModelPermissions(DjangoModelPermissions):
