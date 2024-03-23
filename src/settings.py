060091891646d41ac925e20613f6bc648c6d8701
Aurora Navas <73229219+auroranavas@users.noreply.github.com>
2024-03-23 14:20:45 +0100

    fix: bigger throttle value

--- src/settings.py
+++ src/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
     ),
     # The request per minute must be studied
     "DEFAULT_THROTTLE_RATES": {
-        "anon": "10/minute",  # 10 requests per minute for anonymous
-        "user": "30/minute",  # 30 requests per minute for users
+        "anon": "100/minute",  # 10 requests per minute for anonymous
+        "user": "100/minute",  # 30 requests per minute for users
     },
 }
 
