34ef3b7adf9c532277586683140d78935a9a318e
mromalde <mromalde@gmail.com>
2024-04-04 11:53:18 +0200

    feat: tests with token Done

--- nexong/api/Authentication/tests.py
+++ nexong/api/Authentication/tests.py
@@ -1,5 +1,6 @@
 from nexong.api.Authentication.views import *
 from nexong.models import *
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -42,10 +43,15 @@ class VolunteerApiViewSetTestCase(APITestCase):
             "end_date": "1956-07-05",
         }
         add_files_to_volunteer_data(self)
+        self.user = User.objects.create(username='testuser', email = "example@gmail.com", role=ADMIN)
+        self.token = Token.objects.create(user=self.user)
+
+
+
 
     def test_create_volunteer(self):
         response = self.client.post(
-            "/api/volunteer/", self.volunteer_data, format="multipart"
+            "/api/volunteer/", self.volunteer_data, format="multipart", HTTP_AUTHORIZATION=f'Token {self.token.key}'
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Volunteer.objects.count(), 1)
@@ -53,7 +59,7 @@ class VolunteerApiViewSetTestCase(APITestCase):
 
     def test_retrieve_volunteer(self):
         volunteer = Volunteer.objects.create(**self.volunteer_data)
-        response = self.client.get(f"/api/volunteer/{volunteer.id}/")
+        response = self.client.get(f"/api/volunteer/{volunteer.id}/", HTTP_AUTHORIZATION=f'Token {self.token.key}')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["academic_formation"], "Test formation")
 
@@ -62,7 +68,7 @@ class VolunteerApiViewSetTestCase(APITestCase):
         add_files_to_volunteer_data(self)
         self.volunteer_data["academic_formation"] = "Updated formation"
         response = self.client.put(
-            f"/api/volunteer/{volunteer.id}/", self.volunteer_data
+            f"/api/volunteer/{volunteer.id}/", self.volunteer_data, HTTP_AUTHORIZATION=f'Token {self.token.key}'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
@@ -71,6 +77,6 @@ class VolunteerApiViewSetTestCase(APITestCase):
 
     def test_delete_volunteer(self):
         volunteer = Volunteer.objects.create(**self.volunteer_data)
-        response = self.client.delete(f"/api/volunteer/{volunteer.id}/")
+        response = self.client.delete(f"/api/volunteer/{volunteer.id}/", HTTP_AUTHORIZATION=f'Token {self.token.key}')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Volunteer.objects.count(), 0)
--- nexong/api/permissions.py
+++ nexong/api/permissions.py
@@ -41,7 +41,7 @@ class isAdminGetAndDelete(BasePermission):
 class isAdminGetPutAndDelete(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
-            if request.method in ("GET", "PUT", "PATCH", "DELETE"):
+            if request.method in ("GET", "PUT", "PATCH", "DELETE", "POST"):
                 return request.user.role == "ADMIN"
             else:
                 return False
