bd36eb96de02798dcd169164c57c71aba418fdb2
MRomalde <MRomalde@users.noreply.github.com>
2024-04-04 10:18:49 +0000

    refactor: Format code with Black

--- nexong/api/Authentication/tests.py
+++ nexong/api/Authentication/tests.py
@@ -10,21 +10,26 @@ from rest_framework.test import force_authenticate
 class PartnerApiViewSetTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.user = User.objects.create(username = 'testuser', email = "example2@gmail.com", role = ADMIN)
+        self.user = User.objects.create(
+            username="testuser", email="example2@gmail.com", role=ADMIN
+        )
         self.token = Token.objects.create(user=self.user)
 
     def test_create_partner(self):
         initial_count = Partner.objects.count()
         response = self.client.post(
-            "/api/partner/", {"address": "123 Main St", "birthdate": "1990-01-01"},
-            HTTP_AUTHORIZATION=f'Token {self.token.key}'
+            "/api/partner/",
+            {"address": "123 Main St", "birthdate": "1990-01-01"},
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
         )
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Partner.objects.count(), initial_count + 1)
 
     def test_retrieve_partner(self):
         partner = Partner.objects.create(address="456 Elm St", birthdate="1995-05-05")
-        response = self.client.get(f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.get(
+            f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f"Token {self.token.key}"
+        )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["address"], "456 Elm St")
 
@@ -39,7 +44,7 @@ class PartnerApiViewSetTestCase(TestCase):
                 "birthdate": "2000-10-10",
             },
             content_type="application/json",
-            HTTP_AUTHORIZATION=f'Token {self.token.key}'
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
         )
         self.assertEqual(response.status_code, 200)
         partner.refresh_from_db()
@@ -48,6 +53,8 @@ class PartnerApiViewSetTestCase(TestCase):
     def test_delete_partner(self):
         partner = Partner.objects.create(address="321 Maple St", birthdate="1970-12-12")
         initial_count = Partner.objects.count()
-        response = self.client.delete(f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.delete(
+            f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f"Token {self.token.key}"
+        )
         self.assertEqual(response.status_code, 204)
         self.assertEqual(Partner.objects.count(), initial_count - 1)
