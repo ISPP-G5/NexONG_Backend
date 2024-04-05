5e8963cf2e925de1c8af4f056cd867a542cd1f4b
manortbla <manortbla@alum.us.es>
2024-04-05 12:21:35 +0200

    feat: Schedule CRUD and limit character max

---
+++ nexong/api/Schedule/scheduleSerializer.py
@@ -0,0 +1,23 @@
+from nexong.models import Schedule
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from datetime import *
+from nexong.api.helpers.serializerValidators import timezone
+from django.core.exceptions import ValidationError
+
+
+
+class ScheduleSerializer(ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = "__all__"
+    
+    def validate(self,attrs):
+        start_time = attrs.get("start_time")
+        end_time = attrs.get("end_time")
+        if end_time <= start_time:
+            error_message = {"end_time_error": ["The end time must be after start time."]}
+            raise serializers.ValidationError(error_message)
+        
+        return attrs
