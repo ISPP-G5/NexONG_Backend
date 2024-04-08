90b9ee53c2ad6e2e5bb899e5acc54c16828caeae
pabpercab1 <pabpercab1@users.noreply.github.com>
2024-04-08 09:53:49 +0000

    refactor: Format code with Black

--- nexong/api/helpers/partnerExports.py
+++ nexong/api/helpers/partnerExports.py
@@ -23,16 +23,16 @@ def CreateTableFromResponse(table_data, Story, doc):
     table.setStyle(
         TableStyle(
             [
-            ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-            ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-            ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-            ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-            ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-            ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-            ("GRID", (0, 0), (-1, -1), 1, colors.black),
-             ]
+                ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+                ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+                ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+                ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
+                ("GRID", (0, 0), (-1, -1), 1, colors.black),
+            ]
         )
-     )
+    )
 
     # Table to Story
     Story.append(table)
