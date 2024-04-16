d0f4163a212cefae6f99b1192d75f40974a8f8e1
mromalde <mromalde@gmail.com>
2024-04-16 12:35:25 +0200

    feat: Added partner tests

--- nexong/api/Authentication/tests.py
+++ nexong/api/Authentication/tests.py
@@ -1,49 +1,6 @@
 from django.test import TestCase
 from nexong.api.Authentication.views import *
 from nexong.models import *
-<<<<<<< HEAD
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIRequestFactory
-from rest_framework import status
-from rest_framework.test import force_authenticate
-
-
-class PartnerApiViewSetTestCase(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.user = User.objects.create(
-            username="testuser", email="example2@gmail.com", role=ADMIN
-        )
-        self.token = Token.objects.create(user=self.user)
-
-    def test_create_partner(self):
-        initial_count = Partner.objects.count()
-        response = self.client.post(
-            "/api/partner/",
-            {"address": "123 Main St", "birthdate": "1990-01-01"},
-            HTTP_AUTHORIZATION=f"Token {self.token.key}",
-        )
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(Partner.objects.count(), initial_count + 1)
-
-    def test_retrieve_partner(self):
-        partner = Partner.objects.create(address="456 Elm St", birthdate="1995-05-05")
-        response = self.client.get(
-            f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["address"], "456 Elm St")
-
-    def test_update_partner(self):
-        partner = Partner.objects.create(address="789 Oak St", birthdate="2000-10-10")
-        response = self.client.put(
-            f"/api/partner/{partner.id}/",
-            data={
-                "id": partner.id,
-                "address": "789 Oak St",
-                "enrollment_document": None,
-                "birthdate": "2000-10-10",
-=======
 from rest_framework.test import APIRequestFactory
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -110,25 +67,10 @@ class AdminUserApiViewSetTestCase(TestCase):
                 "is_agreed": False,
                 "terms_version_accepted": 1.0,
                 "family": self.family.id,
->>>>>>> 08ab84542bde34c9745ea93106f2fe7b2a2d8171
             },
             content_type="application/json",
             HTTP_AUTHORIZATION=f"Token {self.token.key}",
         )
-<<<<<<< HEAD
-        self.assertEqual(response.status_code, 200)
-        partner.refresh_from_db()
-        self.assertEqual(partner.address, "789 Oak St")
-
-    def test_delete_partner(self):
-        partner = Partner.objects.create(address="321 Maple St", birthdate="1970-12-12")
-        initial_count = Partner.objects.count()
-        response = self.client.delete(
-            f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(Partner.objects.count(), initial_count - 1)
-=======
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.userfamily.refresh_from_db()
         self.assertEqual(self.userfamily.username, "testuser3")
@@ -209,4 +151,34 @@ class AdminUserApiViewSetTestCase(TestCase):
             HTTP_AUTHORIZATION=f"Token {self.token.key}",
         )
         self.assertEqual(response.status_code, 204)
->>>>>>> 08ab84542bde34c9745ea93106f2fe7b2a2d8171
+
+class PartnerApiViewSetTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.partner = Partner.objects.create(address="789 Oak St", birthdate="2000-10-10", description="MONDONGO")
+        self.user = User.objects.create(username = 'testuser', email = "example2@gmail.com", role = PARTNER, partner=self.partner)
+        self.token = Token.objects.create(user=self.user)
+
+    def test_update_partner(self):
+        partner = Partner.objects.create(address="789 Oak St", birthdate="2000-10-10")
+        response = self.client.put(
+            f"/api/partner/{partner.id}/",
+            data={
+                "id": partner.id,
+                "address": "789 Oak St",
+                "enrollment_document": None,
+                "description": "Mondongo",
+                "birthdate": "2000-10-10",
+            },
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f'Token {self.token.key}'
+        )
+        self.assertEqual(response.status_code, 200)
+        partner.refresh_from_db()
+
+    def test_delete_partner(self):
+        partner = Partner.objects.create(address="321 Maple St", birthdate="1970-12-12")
+        initial_count = Partner.objects.count()
+        response = self.client.delete(f"/api/partner/{partner.id}/", HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Partner.objects.count(), initial_count - 1)
--- nexong/api/Meeting/tests.py
+++ nexong/api/Meeting/tests.py
@@ -62,3 +62,70 @@ class AdminMeetingApiViewSetTestCase(TestCase):
             HTTP_AUTHORIZATION=f"Token {self.token.key}",
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class PartnerMeetingApiViewSetTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.partner3 = Partner.objects.create(
+            description="testdeprueba8", address="333 Elm St", birthdate="1996-05-05"
+        )
+        self.user2 = User.objects.create(
+            username="testuser", email="example1@gmail.com", role=PARTNER, partner=self.partner3
+        )
+        self.token = Token.objects.create(user=self.user2)
+        
+        self.partner4 = Partner.objects.create(
+            description="testdeprueba10",
+            address="333 Elmlos St",
+            birthdate="1997-05-05",
+        )
+        self.meeting2 = Meeting.objects.create(
+            name="Papi", description="Chulo", date="2025-01-22", time="17:50-00:00"
+        )
+        self.meeting2.attendees.add(self.partner3, self.partner4)
+
+    def test_obtain_meeting_by_partner(self):
+        response = self.client.get(
+            f"/api/meeting/{self.meeting2.id}/",
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_meeting_by_partner(self):
+        attendees_ids = [self.partner3.id, self.partner4.id]
+        response = self.client.put(
+            f"/api/meeting/{self.meeting2.id}/",
+            data={
+                "name": "PruebaPapi",
+                "description": "Prueba Chulo",
+                "date": "2025-01-22",
+                "time": "17:50-00:00",
+                "attendees": attendees_ids,
+                "url": f"http://localhost:8000/api/meeting/{self.meeting2.id}/"
+            },
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_meeting_by_partner(self):
+        attendees_ids = [self.partner3.id, self.partner4.id]
+        response = self.client.post(
+            f"/api/meeting/",
+            data={
+                "name": "Prueba3",
+                "description": "Prueba meeting3",
+                "date": "2025-01-22",
+                "time": "17:50-00:00",
+                "attendees": attendees_ids,
+            },
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_meeting_by_partner(self):
+        response = self.client.delete(
+            f"/api/meeting/{self.meeting2.id}/",
+            HTTP_AUTHORIZATION=f"Token {self.token.key}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
