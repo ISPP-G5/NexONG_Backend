19082af83403767e3a79cec7ac52509d9964f11c
JuanluRM <JuanluRM@users.noreply.github.com>
2024-04-17 21:12:09 +0000

    refactor: Format code with Black

--- nexong/api/Lesson/tests.py
+++ nexong/api/Lesson/tests.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
+
 class AdminLessonApiViewSetTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
