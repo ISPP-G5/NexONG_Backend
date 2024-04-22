4a0e70f3f06a83f4b3493f72e9805bfaf4de5191
Aurora Navas <73229219+auroranavas@users.noreply.github.com>
2024-04-22 18:26:47 +0200

    fix: remove unnecessary stuff

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,6 @@ import io
 from google.cloud import secretmanager
 from google.oauth2 import service_account
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
@@ -15,46 +14,36 @@ env_file = os.path.join(BASE_DIR, ".env")
 env.read_env(env_file)
 
 if os.path.isfile(env_file):
-    # read a local .env file
     env.read_env(env_file)
 elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # pull .env file from Secret Manager
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
     client = secretmanager.SecretManagerServiceClient()
     settings_name = os.environ.get("SETTINGS_NAME", "django-settings")
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
     env.read_env(io.StringIO(payload))
 else:
     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
 APPENGINE_URL = env("APPENGINE_URL", default=None)
 FRONTEND_URL = env("FRONTEND_URL", default="http://localhost:3000")
 if APPENGINE_URL:
-    # ensure a scheme is present in the URL before it's processed.
     if not urlparse(APPENGINE_URL).scheme:
         APPENGINE_URL = f"https://{APPENGINE_URL}"
-    
+
     ALLOWED_HOSTS = [urlparse(APPENGINE_URL).netloc]
     CSRF_TRUSTED_ORIGINS = [APPENGINE_URL]
     CORS_ALLOWED_ORIGINS = [FRONTEND_URL]
     CORS_ALLOW_CREDENTIALS = True
-    
+
 else:
     ALLOWED_HOSTS = ["*"]
     CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
-    
+
 
 APPEND_SLASH = True
 
@@ -115,10 +104,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "src.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 if DEBUG == 1:
     DATABASES = {
         "default": {
@@ -132,10 +117,6 @@ if DEBUG == 1:
     }
 else:
     DATABASES = {"default": env.db()}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -152,10 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -164,9 +141,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 if DEBUG == 0:
     GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
         os.path.join(BASE_DIR, "gcpCredentials.json")
@@ -178,30 +152,22 @@ if DEBUG == 0:
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "nexong.User"
 
 REST_FRAMEWORK = {
-    # Uncomment to only let authenticated to our api
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",  # Do not delete this comma, it breaks things somehow....
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_THROTTLE_CLASSES": (
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ),
-    # The request per minute must be studied
     "DEFAULT_THROTTLE_RATES": {
-        "anon": "100/minute",  # 10 requests per minute for anonymous
-        "user": "300/minute",  # 30 requests per minute for users
+        "anon": "100/minute",
+        "user": "300/minute",
     },
 }
 
@@ -216,25 +182,15 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
-    # "SIGNING_KEY": env("SIGNING_KEY"),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
 }
 
-if DEBUG == 1:
-    white_list = [
-        "http://127.0.0.1:3000/iniciar-sesion/",
-    ]
-else:
-    white_list = [env("FRONTEND_URL") + "/iniciar-sesion/"]
-
 DJOSER = {
     "LOGIN_FIELD": "email",
-    # "USER_CREATE_PASSWORD_RETYPE": True,
     "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
     "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
-    # "SEND_CONFIRMATION_EMAIL": True,
     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
     "SET_PASSWORD_RETYPE": True,
     "PASSWORD_RESET_CONFIRM_RETYPE": True,
@@ -246,17 +202,11 @@ DJOSER = {
         "user": "nexong.api.Authentication.authSerializer.UserSerializer",
         "current_user": "nexong.api.Authentication.authSerializer.UserSerializer",
     },
-    "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
-    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": white_list,
     "EMAIL": {
         "activation": "nexong.email.ActivationEmail",
     },
 }
 
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env("GOOGLE_OAUTH2_KEY")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env("GOOGLE_OAUTH2_SECRET")
-# Stripe
 STRIPE_PUBLIC_KEY = env("STRIPE_PUBLIC_KEY")
 STRIPE_PRIVATE_KEY = env("STRIPE_PRIVATE_KEY")
 
@@ -264,7 +214,6 @@ STRIPE_PRIVATE_KEY = env("STRIPE_PRIVATE_KEY")
 MEDIA_URL = "NexONG_Backend/files/"
 MEDIA_ROOT = "files"
 
-# Configuracion de email
 DEFAULT_FROM_EMAIL = "info.nexong@gmail.com"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
@@ -272,5 +221,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
 EMAIL_HOST_PASSWORD = "hrge nkbr uapt oyxk"
-
-
